7542f90a53154132606ab9c6299ecaba02841621
Sadik Sadikov <sadikscorpion@gmail.com>
2024-02-17 10:57:05 +0200

    Ability System Component and Attribute Set

---
+++ Source/StrategyGame/Public/AbilitySystem/ValAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "ValAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRATEGYGAME_API UValAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
